28605fce9629d700f30aeccc9be2b6d4e825a4d6
liyk <>
2021-05-13 18:50:25 +0800

    no message

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -932,14 +932,14 @@ export default {
         treeData.forEach((e, i) => {
           const data = redata.nearDevice
           if (e.deviceId === redata.nearDevice.deviceId) {
-            // e.deviceName = data.deviceName
-            // e.deviceId = data.deviceId
-            // e.id = data.id
-            // e.deviceAddress = data.deviceAddress
-            // e.treeId = data.deviceId
-            // e.treeIndex = i
+            e.deviceName = data.deviceName
+            e.deviceId = data.deviceId
+            e.id = data.id
+            e.deviceAddress = data.deviceAddress
+            e.treeId = data.deviceId
+            e.treeIndex = i
             e.online = data.online
-            // e.shanshuo = data.shanshuo
+            e.shanshuo = data.shanshuo
           }
         })
         this.treeData = treeData
